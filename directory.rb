49d14077869b55d829029b429b5c830a259d6b7b
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-12 02:07:17 +0100

    Rename print to print_student_list

--- directory.rb
+++ directory.rb
@@ -33,7 +33,7 @@ def print_header
   puts "-------------"
 end
 
-def print
+def print_student_list
   @students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -58,7 +58,7 @@ end
 
 def show_students
   print_header
-  print
+  print_student_list
   print_footer
 end
 
